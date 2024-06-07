9d0212984e44d527b34c6c01f7c97b3d71635841
goldipl <mgodlewski92@gmail.com>
2024-06-07 09:45:35 +0200

    add hamburger script

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Hamburger menu
+$(document).ready(function(){
+	$('.header-2-0 .hamburger-menu').click(function(){
+		$(this).toggleClass('open');
+	});
+});                                                                         
