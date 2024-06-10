3dcc8a94927ca0a112995bbf0dd6c978c962e9a7
goldipl <mgodlewski92@gmail.com>
2024-06-10 15:26:15 +0200

    add new sale swiper settings

--- js/home-swipers.js
+++ js/home-swipers.js
@@ -1,22 +1,47 @@
-const swiper = new Swiper('.hero-swiper', {
-    // Optional parameters
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.hero-swiper', {
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next.hero-swiper-next',
+      prevEl: '.swiper-button-prev.hero-swiper-prev',
     },
-
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
     },
-
   });
+
+  const sale_swiper = new Swiper('.sale-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 6,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next.sale-swiper-next',
+      prevEl: '.swiper-button-prev.sale-swiper-prev',
+    },
+    breakpoints: {
+      1240: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      760: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      200: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+    },
+  });
+});
