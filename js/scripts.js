b83e229c5e707815fb8d90cad8035ef8d0200ffa
goldipl <mgodlewski92@gmail.com>
2024-06-11 20:36:05 +0200

    add currentYear script

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // Hamburger menu
 $(document).ready(function(){
+	const currentYear = new Date().getFullYear();
 	$('.header-2-0 .hamburger-menu').click(function(){
 		$(this).toggleClass('open');
 	});
+	$('footer .footer-wrapper-bottom span').text(currentYear);
 });                                                                         
