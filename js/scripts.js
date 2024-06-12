9adbd66f63b5e657a2a833752f9f0e9d58a547cf
goldipl <mgodlewski92@gmail.com>
2024-06-12 19:20:25 +0200

    add contact template in footer with styles, handle also toggling button showing more numbers

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 	$('footer .footer-wrapper-bottom span').text(currentYear);
-});                                                                         
+});                                                                        
+
+// Toggling show more telephones on desktop
+$(document).ready(function() {
+	$(".contact-slot__show-more").click(function() {
+		var hiddenElements = $(this).closest(".contact-slot.phones").find(".contact-slot-line.phone");
+		hiddenElements.toggleClass("hidden");
+
+		$(".contact-slot__show-more span").each(function() {
+			var showMoreText = $(this).text().trim().includes("więcej") ? "Pokaż mniej" : "Pokaż więcej";
+			$(this).text(showMoreText);
+		});
+		
+		$(this).find(".arrow-down-show").toggleClass("rotated");
+	});
+});
