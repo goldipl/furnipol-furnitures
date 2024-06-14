1bf02796246eabff5d6a0b5e011be25805b7355b
goldipl <mgodlewski92@gmail.com>
2024-06-14 11:25:30 +0200

    rollback to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 // Hamburger menu
-document.addEventListener('DOMContentLoaded', () => {
-    const currentYear = new Date().getFullYear();
-    document.querySelector('.header-2-0 .hamburger-menu').addEventListener('click', function() {
-        this.classList.toggle('open');
-    });
-
-    document.querySelector('footer .footer-wrapper-bottom span').textContent = currentYear;
-});
+$(document).ready(function(){
+	const currentYear = new Date().getFullYear();
+	$('.header-2-0 .hamburger-menu').click(function(){
+		$(this).toggleClass('open');
+	});
+	$('footer .footer-wrapper-bottom span').text(currentYear);
+});                                                                        
 
 // Toggling show more telephones on desktop
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll(".contact-slot__show-more").forEach(button => {
-        button.addEventListener('click', () => {
-            const hiddenElements = button.closest(".contact-slot.phones").querySelectorAll(".contact-slot-line.phone");
-            hiddenElements.forEach(element => element.classList.toggle("hidden"));
+$(document).ready(function() {
+	$(".contact-slot__show-more").click(function() {
+		var hiddenElements = $(this).closest(".contact-slot.phones").find(".contact-slot-line.phone");
+		hiddenElements.toggleClass("hidden");
 
-            document.querySelectorAll(".contact-slot__show-more span").forEach(span => {
-                const showMoreText = span.textContent.trim().includes("więcej") ? "Pokaż mniej" : "Pokaż więcej";
-                span.textContent = showMoreText;
-            });
-
-            button.querySelector(".arrow-down-show").classList.toggle("rotated");
-        });
-    });
+		$(".contact-slot__show-more span").each(function() {
+			var showMoreText = $(this).text().trim().includes("więcej") ? "Pokaż mniej" : "Pokaż więcej";
+			$(this).text(showMoreText);
+		});
+		
+		$(this).find(".arrow-down-show").toggleClass("rotated");
+	});
 });
